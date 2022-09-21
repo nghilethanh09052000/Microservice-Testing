7c7f51ce4417b9be900a2e28bbfd8fec05f580d6
Your Name <nghilt19411@gmail.com>
2022-09-21 17:10:05 +0700

    fix dockerhub

--- query/index.js
+++ query/index.js
@@ -20,14 +20,9 @@ const posts = {};
 //     }
 // }
 
-app.get("/posts", (req, res) => {
-    res.send(posts)
-});
-
-app.post("/events", (req, res) => {
-    const { type, data } = req.body;
-    switch (type) 
-    {
+function handleEvent(type,data)
+{
+    switch (type) {
         case "PostCreated":
             {
                 const { id, title } = data;
@@ -36,19 +31,43 @@ app.post("/events", (req, res) => {
             break;
         case "CommentCreated":
             {
-                const { id, content, postId } = data;
+                const { id, content, postId, status } = data;
                 const post = posts[postId];
-                post.comments.push({ id, content });
+                post.comments.push({ id, content, status });
             }
             break;
-
+        case "CommentUpdated":
+            {
+                const { id, content, postId, status } = data;
+                const post = posts[postId];
+                const comment = post.comments.find((comment) => comment.id === id);
+                comment.status = status;
+                comment.content = content;
+            }
+            break;
         default:
             throw new Error("Nothing to do");
     }
-    console.log(post)
+}
+app.get("/posts", (req, res) => {
+    res.send(posts);
+});
+
+app.post("/events", (req, res) => {
+    const { type, data } = req.body;
+    handleEvent(type,data)
     res.send({});
 });
 
-app.listen(4002, () => {
-    console.log("Listen on 4002");
+app.listen(4002, async () => {
+    console.log("Listening on 4002");
+    try {
+        const res = await axios.get("http://localhost:4005/events");
+        for (let event of res.data) {
+            console.log("Processing event:", event.type);
+            handleEvent(event.type, event.data);
+        }
+    } catch (error) {
+        console.log(error.message);
+    }
 });
